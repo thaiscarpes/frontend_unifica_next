611236668b8451eb6835c1bbc52e6bbc87b50640
thaiscarpes <thaiscarpes@hotmail.com>
2023-10-26 23:53:27 -0300

    listagem editada

--- app/(pages)/location/[id]/page.js
+++ app/(pages)/location/[id]/page.js
@@ -1,44 +1,68 @@
+'use client'
 import DataListItem from '@/components/dataListItem'
 import Footer from '@/components/footer'
+import LoadSpinner from '@/components/loadSpinner'
+import { fetchLocation } from '@/services/fetchData'
 import Image from 'next/image'
-import { MdLocationOn, MdFax, MdCall, MdAccessTime } from 'react-icons/md'
+import { useEffect, useState } from 'react'
+import { MdAccessTime, MdCall, MdFax, MdLocationOn } from 'react-icons/md'
 
-export default async function Location({params}) {
+export default function Location({ params }) {
 
-  const res = await fetch(`http://localhost:3000/api/locations/${params.id}`)
-  const data = await res.json()
+  const [data, setData] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      const response = await fetchLocation(params.id)
+      if (response) {
+        setData(response[0])
+        console.log(data);
+      }
+    }
+    fetchDataAsync()
+    setIsLoading(false)
+  }, [])
 
   return (
-    <main className="flex flex-col gap-8 w-full mt-[148px] px-4 py-12 mb-14 relative md:px-[30%]">
+    <>{isLoading ? (<LoadSpinner />) : (
 
-      <div className="flex flex-col gap-1">
-        <h2 className="font-semibold text-zinc-800 text-2xl">{data[0].title}</h2>
-        <span className="text-base">{data[0].description}</span>
-      </div>
+      <main className="flex flex-col gap-8 w-full mt-[148px] px-4 py-12 mb-14 relative md:px-[30%]">
 
-      <Image src={`https://source.unsplash.com/${data[0].image}`} alt={data[0].title} width={512} height={512} className="rounded-lg max-h-[180px] w-full object-cover" />
+        <div className="flex flex-col gap-1">
+          <h2 className="font-semibold text-zinc-800 text-2xl">{data.title}</h2>
+          <span className="text-base">{data.description}</span>
+        </div>
 
-      <DataListItem label="Endereço" content={data[0].address}>
-        <MdLocationOn className="text-lg"/>
-      </DataListItem>
+        <Image src={`https://source.unsplash.com/${data.image}`} alt={data.title} width={512} height={512} className="rounded-lg max-h-[180px] w-full object-cover" />
 
-      <DataListItem label="Ramal" content={data[0].extension}>
-        <MdFax className="text-lg"/>
-      </DataListItem>
+        {data.address &&
+          <DataListItem label="Endereço" content={data.address}>
+            <MdLocationOn className="text-lg" />
+          </DataListItem>
+        }
 
-      { data[0].phone &&
-      <DataListItem label="Telefone" content={data[0].phone}>
-        <MdCall className="text-lg"/>
-      </DataListItem>
-      }
+        {data.extension &&
+          <DataListItem label="Ramal" content={data.extension}>
+            <MdFax className="text-lg" />
+          </DataListItem>
+        }
 
-      <DataListItem pills={true} message="Alguns estabelecimentos podem ter horários de intervalo de duas horas." label="Horários de funcionamento"
-        startJourney={data[0].startJourney}
-        endJourney={data[0].endJourney}>
-        <MdAccessTime className="text-lg"/>
-      </DataListItem>
+        {data.phone &&
+          <DataListItem label="Telefone" content={data.phone}>
+            <MdCall className="text-lg" />
+          </DataListItem>
+        }
 
-      <Footer locationId={data[0]._id} />
-    </main>
-  )
+        <DataListItem pills={true} message="Alguns estabelecimentos podem ter horários de intervalo de duas horas." label="Horários de funcionamento"
+          startJourney={data.startJourney}
+          endJourney={data.endJourney}>
+          <MdAccessTime className="text-lg" />
+        </DataListItem>
+
+        <Footer locationId={data._id} />
+      </main>
+
+    )}</>
+  )
 }
