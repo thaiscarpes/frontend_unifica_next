55ded8d0d91df34fc45b5be6d42244d5e4c673a0
thaiscarpes <thaiscarpes@hotmail.com>
2023-11-21 21:04:50 -0300

    anotação de implementação futura

--- next.config.js
+++ next.config.js
@@ -1,26 +1,47 @@
 /** @type {import('next').NextConfig} */
 
 //IMPORTA O NEXT-PWA PARA TRANSFORMAR A APLICAÇÃO EM UM PWA
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
 
 const nextConfig = {
   //DEFINIÇÃO DAS OPÇOES DO PWA
   ...withPWA({
-    dest: 'public',
+    dest: "public",
     register: true,
-    skipWaiting: true
+    skipWaiting: true,
   }),
   images: {
     //DEFINE AS ORIGENS DE FORA DA APLICAÇÃO ONDE SERÃO CHAMADAS IMAGENS EXTERNAS
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "source.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-}
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: 'frontend-unifica-next.vercel.app' }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
