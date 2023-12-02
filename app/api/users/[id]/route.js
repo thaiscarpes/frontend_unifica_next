3a0e9f9cbc37fb76447ca0690d3b737a6ad048a4
thaiscarpes <thaiscarpes@hotmail.com>
2023-12-02 17:53:24 -0300

    add delete user function

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,11 +1,23 @@
 import UserModel from '@/utils/userModel'
 
-//LISTAR UMA LOCALIZAÇÃO ESPECÍFICA
+//LISTAR UM USUARIO ESPECÍFICO
 export async function GET(request, { params }) {
     const email = params.id
 
     try {
         const user = await UserModel.find({ email: email })
+        return Response.json(user)
+    } catch (error) {
+        return Response.json({ message: `Error: ${error}` })
+    }
+}
+
+//DELETAR UM USUARIO 
+export async function DELETE(request, { params }) {
+    const email = params.id
+
+    try {
+        const user = await UserModel.deleteOne({_id: id})
         return Response.json(user)
     } catch (error) {
         return Response.json({ message: `Error: ${error}` })
